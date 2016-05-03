29fec53f1b1d41ee03fe7abd1a8c9e9df709f5a3
Tsegorah1 <spear19930@yandex.ru>
2016-05-03 10:41:33 +0300

    Ничего интересного.

--- app/src/main/java/com/example/vkapp1/MySimpleCursorAdapter.java
+++ app/src/main/java/com/example/vkapp1/MySimpleCursorAdapter.java
@@ -2,6 +2,7 @@ package com.example.vkapp1;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.SimpleCursorAdapter;
 
@@ -16,7 +17,35 @@ public class MySimpleCursorAdapter extends SimpleCursorAdapter {
 
     @Override
     public void setViewImage(ImageView v, String value) {
-        super.setViewImage(v, value);
+        String uri;
+        Log.e("log", "====================== setViewImage");/*
+        switch (this.getCursor().getInt(4)) {
+            case 0:
+                Log.e("log", "======================            0");
+                uri = "@drawable/neload_icon.png";
+                break;
+            case 2:
+                Log.e("log", "======================            2");
+                uri = "@drawable/delete_icon.png";
+                break;
+            default:
+                Log.e("log", "======================            default");
+                uri = "@drawable/reload_icon.png";
+        }*/
+        super.setViewImage(v, value);//uri);
+        switch (this.getCursor().getInt(4)) {
+            case 0:
+                Log.e("log", "======================            0");
+                v.setImageResource(R.drawable.neload_icon);;
+                break;
+            case 2:
+                Log.e("log", "======================            2");
+                v.setImageResource(R.drawable.delete_icon);
+                break;
+            default:
+                Log.e("log", "======================            default");
+                v.setImageResource(R.drawable.reload_icon);
+        }
         //Cursor c = db.getByID();
         //final ImageView w = v;
 /*
